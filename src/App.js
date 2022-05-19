1bf9295db07ccc226da9b589b4689c7116d196ff
Ismail51 <dassler-ben@hotmail.fr>
2022-05-19 13:55:41 +0200

    createRoute

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react"
-import './App.css';
-import Navbar from './components/Navbar';
+import Profil from "./components/profil";
+import ActiveOffres from "./components/activeOffres";
+import Messagerie from "./components/messagerie";
+import Navbar from "./components/navbar";
+import RechercheBar from "./components/rechercheBar";
+import ServicesRendu from "./components/servicesRendu";
+import VoirPlus from "./components/voirPlus";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/Profil" element={<Profil />}></Route>
+        <Route path="/ActivesOffres" element={<ActiveOffres />}></Route>
+        <Route path="/Messagerie" element={<Messagerie />}></Route>
+        <Route path="/Navbar" element={<Navbar />}></Route>
+        <Route path="/RechercheBar" element={<RechercheBar />}></Route>
+        <Route path="/ServicesRendu" element={<ServicesRendu />}></Route> */}
+        <Route path="/VoirPlus" element={<VoirPlus />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/components/activeOffres.jsx
@@ -0,0 +1,5 @@
+export default function ActiveOffres() {
+  return (
+    <p>page profil</p>
+  );
+}
