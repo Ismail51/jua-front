d2893be0c99cacd1e018fe1952dc224628715f11
naim edoo <naim.e2703@gmail.com>
2022-05-24 10:39:52 +0200

    voir plus page done

--- src/components/Recherche.jsx
+++ src/components/Recherche.jsx
@@ -4,64 +4,53 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { NavLink } from 'react-router-dom'
 
 export default function Recherche() {
-  return (
-    <div className="main-div">
-            <Navbar/>
-                <div className="search">
-                    <div className="search-div">
-                        <label htmlFor="lists">Liste des taches</label>
-                            <select id="lists">
-                            <option  disabled selected value> -- select an option -- </option>
-                            <option value="menage">Menage</option>
-                            <option value="demanage">Demenage</option>
-                            <option value="informatiqe">Informatique</option>
-                            <option value="accompagne">Accompagne</option>
-                            </select>
-                            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
-                    </div>
-                    <div className="div-option">
-                      <h3>27 result pour menage</h3>
-                    </div>
-                    
-                    <div className="container">
-                        
-                        <div className="card-body">
-                            <img src="./image/jean.jpg" alt="profile"/>
-                            <div>
-                                <h3>jean micheal</h3>
-                                <h4>Menage-3heures</h4>
-                                <p>“Je suis a la recherche d’une personne pouvant m’aider a bl Je suis a la recherche d’une personne pouvant m’aider a blaa
-                                Je suis a la recherche d’une personne pouvant m’aider a ba”</p>
-                                <div className="btn">
-                                    <button><NavLink to="/VoirPlus">Voir plus</NavLink></button>
-                                </div>
-                            </div>
+    return (
+        <div className="main-div">
+            <Navbar />
+            <div className="search">
+                <div className="search-div">
+                    <label htmlFor="lists">Liste des taches</label>
+                    <select id="lists">
+                        <option disabled selected value> -- select an option -- </option>
+                        <option value="menage">Menage</option>
+                        <option value="demanage">Travaux d'intérieur</option>
+                        <option value="informatiqe">Informatique</option>
+                        <option value="accompagne">Transport</option>
+                        <option value="accompagne">Divers</option>
+                    </select>
+                </div >
+            </div >
+            <div className="div-option">
+                <h3>27 offres pour le menage</h3>
+            </div>
+
+            <div className="container">
+
+                <div className="card-body">
+                    <img src="./image/jean.jpg" alt="profile" />
+                    <div>
+                        <h3>jean micheal</h3>
+                        <h4>Menage-3heures</h4>
+                        <p>“Je suis a la recherche d’une personne pouvant m’aider a bl Je suis a la recherche d’une personne pouvant m’aider a blaa
+                            Je suis a la recherche d’une personne pouvant m’aider a ba”</p>
+                        <div className="btn">
+                            <button><NavLink to="/VoirPlus">Voir plus</NavLink></button>
                         </div>
-                        <div className="card-body">
-                            <img src="./image/jean.jpg" alt="profile"/>
-                            <div>
-                                <h3>jean micheal</h3>
-                                <h4>Menage-3heures</h4>
-                                <p>“Je suis a la recherche d’une personne pouvant m’aider a bla Je suis a la recherche d’une personne pouvant m’aider a bla
-                                Je suis a la recherche d’une personne pouvant m’aider a ba”</p>
-                                <div className="btn">
-                                    <button><NavLink to="/VoirPlus">Voir plus</NavLink></button>
-                                </div>
-                            </div>
-                        </div> 
-
+                    </div >
+                </div >
+            </div >
+            <div className="card-body">
+                <img src="./image/jean.jpg" alt="profile" />
+                <div>
+                    <h3>jean micheal</h3>
+                    <h4>Menage-3heures</h4>
+                    <p>“Je suis a la recherche d’une personne pouvant m’aider a bla Je suis a la recherche d’une personne pouvant m’aider a bla
+                        Je suis a la recherche d’une personne pouvant m’aider a ba”</p>
+                    <div className="btn">
+                        <button><NavLink to="/VoirPlus">Voir plus</NavLink></button>
                     </div>
-
-
-
-
-
-
-
-
-                </div>
-       
-    </div>
-    
-  )
+                </div >
+            </div >
+        </div >
+    )
 }
--- src/components/voirPlus.jsx
+++ src/components/voirPlus.jsx
@@ -1,32 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from './Navbar'
 import '../voirPlus.css'
+import { useParams } from "react-router-dom"
+import axios from 'axios'
+import { NavLink } from "react-router-dom";
 
 
 function VoirPlus() {
-  return(
+  let { id } = useParams();
+  const [offer, setOffer] = useState({})
+  const [ok, setOk] = useState(false)
+  useEffect(() => {
+    axios.get(`http://localhost:3002/offers/${id}`).then(data => {
+      setOffer(data.data)
+      setOk(true)
+    })
+  }, [])
+
+  return (
     <div className="main">
       <NavBar></NavBar>
-      <div>
-        <div className="offer-top">
-          <img src="./image/jean.jpg" alt="" />
-          <div className="right-info">      
-            <p>name</p>
-            <p>location</p>
-            <p>date inscription</p>
+      {
+        ok ?
+          <div>
+            <div className="offer-top">
+              <img src="./image/jean.jpg" alt="" />
+              <div className="right-info">
+                <p>Prénom : {offer.created_by.firstnamne}</p>
+                <p>Nom : {offer.created_by.lastname}</p>
+                <p>Ville : {offer.created_by.city}</p>
+                <p>Date : {offer.created_by.registered_at}</p>
+              </div>
+            </div>
+            <div className="offer-bottom">
+              <div className="offer-description">
+                <h3>description</h3>
+                <p>{offer.description}</p>
+              </div>
+              <div className="offer-btn">
+                <button><NavLink to="/recherche">return</NavLink></button>
+                <button>accept</button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="offer-bottom">
-          <div className="offer-description">
-            <h3>description</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus at debitis accusamus, laboriosam consequuntur reiciendis distinctio veritatis cupiditate id quasi?</p>
-          </div>
-          <div className="offer-btn">
-            <button>return</button>
-            <button>accept</button>
-          </div>
-        </div>
-      </div>
+          : null
+      }
     </div>
   )
 }
