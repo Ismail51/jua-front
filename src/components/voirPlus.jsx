82cd3f772ef7f5b0238c790e3885d1bf2fe96212
naim edoo <naim.e2703@gmail.com>
2022-05-30 15:07:03 +0200

    add a css for offers accepted

--- src/components/voirPlus.jsx
+++ src/components/voirPlus.jsx
@@ -7,7 +7,6 @@ import { NavLink, Navigate } from "react-router-dom";
 import { AppContext } from "../App";
 
 
-
 function VoirPlus() {
   let { id } = useParams();
   const [offer, setOffer] = useState({})
@@ -31,18 +30,18 @@ function VoirPlus() {
       setAccepted(true)
     })
   }
-const getDate = (date)=>{
-  const ok = new Date(date)
-  return ok.toLocaleDateString("fr-FR")
-    console.log('okkkk',ok.toLocaleDateString("fr-FR"))
-}
+  const getDate = (date) => {
+    const ok = new Date(date)
+    return ok.toLocaleDateString("fr-FR")
+    console.log('okkkk', ok.toLocaleDateString("fr-FR"))
+  }
   return (
     <div className="main">
       <NavBar></NavBar>
       {
         ok ?
           <div className="offer">
-                        {offer.accepted_by ? <div className="msg-top">L'offre a déja été acceptée</div>: null}
+            {offer.accepted_by ? <div className="msg-top">L'offre a déja été acceptée</div> : null}
 
             <div className="offer-top">
               <img src="/image/aide.jpeg" alt="" />
@@ -56,16 +55,15 @@ const getDate = (date)=>{
             <div className="offer-bottom">
               <div className="offer-description">
                 <h3>Description</h3>
-              <div className="description-offer">
-                <p>{offer.description}</p>
-              </div>
+                <div className="description-offer">
+                  <p>{offer.description}</p>
+                </div>
               </div>
               <div className="offer-btn">
                 <button><NavLink to="/recherche">Retour</NavLink></button>
                 {accepted ? <Navigate to="/recherche" /> : null}
-                {offer.accepted_by ? null: <button onClick={acceptOffer}>Accepter</button>}
+                {offer.accepted_by ? null : <button onClick={acceptOffer}>Accepter</button>}
               </div>
-
             </div>
           </div>
           : null
@@ -74,4 +72,6 @@ const getDate = (date)=>{
   )
 }
 export default VoirPlus
+
+
 
